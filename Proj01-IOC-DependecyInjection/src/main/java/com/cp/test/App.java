bb10a4ec35042fa4f15bf117c5ef6cd0fc1fafe3
Pradeep Kumar <k.pradeep661@gmail.com>
2018-07-30 00:49:20 +0530

    TL-First Commit

---
+++ Proj01-IOC-DependecyInjection/src/main/java/com/cp/test/App.java
@@ -0,0 +1,13 @@
+package com.cp.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
